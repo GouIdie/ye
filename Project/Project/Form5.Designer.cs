323cda01d0d5fbcbf1e24ad1c9361014631a9f67
Thomas Gould <Thomas.Gould@worthing.ac.uk>
2020-04-01 15:12:08 +0100

    dateval

--- Project/Project/Form5.Designer.cs
+++ Project/Project/Form5.Designer.cs
@@ -30,6 +30,17 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -45,6 +56,8 @@
             // 
             // listBox1
             // 
+            this.listBox1.BackColor = System.Drawing.Color.SeaShell;
+            this.listBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(28)))));
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 21;
             this.listBox1.Location = new System.Drawing.Point(12, 39);
@@ -53,12 +66,150 @@
             this.listBox1.TabIndex = 1;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.SeaShell;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.SeaShell;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(28)))));
+            this.button1.Location = new System.Drawing.Point(480, 46);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 45);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "View Data";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.SeaShell;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Location = new System.Drawing.Point(255, 130);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(214, 28);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.SeaShell;
+            this.label2.Location = new System.Drawing.Point(259, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 21);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Date Subscribed";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.SeaShell;
+            this.label3.Location = new System.Drawing.Point(259, 241);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 21);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Description";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.SeaShell;
+            this.label4.Location = new System.Drawing.Point(259, 39);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(154, 21);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Subscription name";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.BackColor = System.Drawing.Color.SeaShell;
+            this.richTextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox2.Location = new System.Drawing.Point(255, 63);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(214, 28);
+            this.richTextBox2.TabIndex = 8;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.BackColor = System.Drawing.Color.SeaShell;
+            this.richTextBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox3.Location = new System.Drawing.Point(255, 265);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(355, 156);
+            this.richTextBox3.TabIndex = 9;
+            this.richTextBox3.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.SeaShell;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.SeaShell;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(28)))));
+            this.button2.Location = new System.Drawing.Point(480, 181);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(130, 45);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Save Changes";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.BackColor = System.Drawing.Color.SeaShell;
+            this.richTextBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox4.Location = new System.Drawing.Point(255, 198);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.Size = new System.Drawing.Size(214, 28);
+            this.richTextBox4.TabIndex = 11;
+            this.richTextBox4.Text = "";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.SeaShell;
+            this.label5.Location = new System.Drawing.Point(259, 174);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 21);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Email ";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.SeaShell;
+            this.button3.FlatAppearance.BorderColor = System.Drawing.Color.SeaShell;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(28)))));
+            this.button3.Location = new System.Drawing.Point(480, 113);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(130, 45);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Edit Data";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // MySub
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(43)))), ((int)(((byte)(45)))), ((int)(((byte)(47)))));
             this.ClientSize = new System.Drawing.Size(622, 442);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.richTextBox4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -76,5 +227,16 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button3;
     }
 }
--- Project/Project/Form5.cs
+++ Project/Project/Form5.cs
@@ -8,12 +8,14 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.OleDb;
+using System.Text.RegularExpressions;
+
 namespace Project
 {
     public partial class MySub : Form
     {
         private OleDbConnection Conn;
-        private OleDbDataReader dr;
+        private OleDbDataReader reader;
         private OleDbCommand cmd;
         private int CusD;
         public MySub(int Cid,OleDbConnection Con)
@@ -22,12 +24,18 @@ namespace Project
             Conn = Con;
             CusD = Cid;
             cmd = new OleDbCommand("", Conn);
+            richTextBox1.Enabled = false;
+            richTextBox2.Enabled = false;
+            richTextBox3.Enabled = false;
+            richTextBox4.Enabled = false;
+           
         }
         
         private void Form5_Load(object sender, EventArgs e)
         {
             LoadData();
             Conn.Open();
+            
         }
         private void LoadData()
         {
@@ -35,19 +43,18 @@ namespace Project
            
             try
             {
-                string query = "SELECT SubscriptionName FROM Subscriptions,CustSub WHERE CustSub.CustomerID="+CusD+"And CustSub.SubscriptionID = Subscriptions.SubscriptionID";
+                string query = "SELECT SubscriptionName FROM Subscriptions,CustSub WHERE CustSub.CustomerID=" + CusD + "And CustSub.SubscriptionID = Subscriptions.SubscriptionID";
                 cmd.CommandText = query;
                 Conn.Open();
-                dr = cmd.ExecuteReader();
-                if (dr.HasRows)
+                reader = cmd.ExecuteReader();
+                if (reader.HasRows)
                 {
-                    while (dr.Read())
+                    while (reader.Read())
                     {
-                        listBox1.Items.Add(dr[0].ToString());
-                
+                        listBox1.Items.Add(reader[0].ToString());                                            
                     }
                 }
-                dr.Close();
+                reader.Close();
                 Conn.Close();
 
             }
@@ -60,6 +67,85 @@ namespace Project
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
 
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            reader = null;
+            string query = "SELECT SubscriptionName,Description,DateAdded FROM Subscriptions WHERE SubscriptionName ='"+listBox1.Text+"'";
+            cmd.CommandText = query;
+            Conn.Open();
+            reader = cmd.ExecuteReader();
+            reader.Read();
+            try { 
+            richTextBox2.Text = reader[0].ToString();
+            richTextBox3.Text = reader[1].ToString();
+            richTextBox1.Text = (reader[2].ToString()).Substring(0, 8);
+            
+            }
+            catch
+            {               
+            }
+            Conn.Close();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {    
+
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void button3_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Enabled = true;
+            richTextBox2.Enabled = true;
+            richTextBox3.Enabled = true;
+            richTextBox4.Enabled = true;
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected bool CheckDate(String date)
+
+        {
+
+            try
+
+            {
+
+                DateTime dt = DateTime.Parse(date);
+
+                return true;
+
+            }
+            catch
+
+            {
+
+                return false;
+
+            }
+
+        }
+        Regex DateLength = new Regex(@"^\d{2}\/\d{2}\/\d{2}$");
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            bool date = CheckDate(richTextBox1.Text);
+            if ((date == true) && (DateLength.IsMatch(richTextBox1.Text)))
+            {
+                MessageBox.Show("This is good");
+            }
+            else
+            {
+                MessageBox.Show("Date must be in the format dd/mm/yy and must be a valid date");
+            }
         }
     }
 }
